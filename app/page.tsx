5f46f072736b38416f733f3d944c668942e7079d
Syed Mehdi <mehdisyed3@gmail.com>
2024-02-13 11:47:13 -0500

    instaled shadc and the button component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1> Let's build disney</h1>
+      <h1 className='text-red-500'> Let's build disney</h1>
     </main>
   );
 }
